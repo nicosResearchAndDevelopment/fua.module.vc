2e8ac4e94279c9656d980f280a0e2c6ff8ed0f1f
Simon Petrac <pfoerdie@gmail.com>
2021-08-23 13:15:26 +0200

    first draft of verifiable credential proofs with JOSE

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # module.vc
 
+```ts
+interface module_vc {
+    VC2JWT(vc: VerifiableCredential, options: { alg?: string, key: JsonWebKey }): Promise<JsonWebToken>;
+    JWT2VC(jwt: JsonWebToken, options: { keys: Array<JsonWebKey> }): Promise<VerifiableCredential>;
+    VP2JWT(vp: VerifiablePresentation, options: { alg?: string, key: JsonWebKey }): Promise<JsonWebToken>;
+    JWT2VP(jwt: JsonWebToken, options: { keys: Array<JsonWebKey> }): Promise<VerifiablePresentation>;
+}
+```
--- test/module.vc.test.js
+++ test/module.vc.test.js
@@ -1,10 +1,47 @@
 const
-    {describe, test} = require('mocha'),
-    expect           = require('expect'),
-    module_vc        = require('../src/module.vc.js');
+    {describe, test, before} = require('mocha'),
+    expect                   = require('expect'),
+    module_vc                = require('../src/module.vc.js'),
+    {generateKeyPair}        = require('jose/util/generate_key_pair'),
+    {fromKeyLike}            = require('jose/jwk/from_key_like'),
+    {decodeProtectedHeader}  = require('jose/util/decode_protected_header'),
+    testVC                   = Object.freeze(require('./data/example-verifiable-credential.json')),
+    testVP                   = Object.freeze(require('./data/example-verifiable-presentation.json'));
 
 describe('module.vc', function () {
 
-    // TODO testing
+    const encryptOptions = {alg: 'RS256'}, decryptOptions = {keys: []};
+    before('generate key pair', async function () {
+        const
+            {privateKey, publicKey} = await generateKeyPair(encryptOptions.alg),
+            [privateJWK, publicJWK] = await Promise.all([privateKey, publicKey].map(fromKeyLike));
+
+        privateJWK.alg     = publicJWK.alg = encryptOptions.alg;
+        privateJWK.kid     = publicJWK.kid = 'did:example:marzipan#key-' + Date.now();
+        encryptOptions.key = Object.freeze(privateJWK);
+        decryptOptions.keys.push(Object.freeze(publicJWK));
+    });
+
+    test('sign a VC as a JWT and verify it to the same VC again', async function () {
+        const jwt = await module_vc.VC2JWT(testVC, encryptOptions);
+        expect(typeof jwt).toBe('string');
+        console.log(jwt);
+        expect(await decodeProtectedHeader(jwt)).toMatchObject({alg: encryptOptions.alg});
+        const vc = await module_vc.JWT2VC(jwt, decryptOptions);
+        expect(typeof vc).toBe('object');
+        console.log(vc);
+        expect(vc).toEqual(testVC);
+    });
+
+    test('sign a VP as a JWT and verify it to the same VP again', async function () {
+        const jwt = await module_vc.VP2JWT(testVP, encryptOptions);
+        expect(typeof jwt).toBe('string');
+        console.log(jwt);
+        expect(await decodeProtectedHeader(jwt)).toMatchObject({alg: encryptOptions.alg});
+        const vp = await module_vc.JWT2VP(jwt, decryptOptions);
+        expect(typeof vp).toBe('object');
+        console.log(vp);
+        expect(vp).toEqual(testVP);
+    });
 
 });
