30e1833a34cf18b9e5bac0e79462ca95820ffd7e
Simon Petrac <pfoerdie@gmail.com>
2021-08-25 10:41:02 +0200

    wrote better tests

--- test/data/certs/index.js
+++ test/data/certs/index.js
@@ -1,24 +1,24 @@
 const
-    { readFileSync } = require('fs'),
-    { join: joinPath } = require('path'),
-    load = (filename) => readFileSync(joinPath(__dirname, filename));
+    {readFileSync}   = require('fs'),
+    {join: joinPath} = require('path'),
+    load             = (filename) => readFileSync(joinPath(__dirname, filename));
 
-exports.server = {
-    private: load('server.key'),
-    public: load('server.key.pub')
+exports.server_1 = {
+    private: load('server_1.key'),
+    public:  load('server_1.key.pub')
 };
 
-exports.server_tls = {
-    private: load('server_tls.key'),
-    public: load('server_tls.key.pub')
+exports.server_2 = {
+    private: load('server_2.key'),
+    public:  load('server_2.key.pub')
 };
 
-exports.client = {
-    private: load('client.key'),
-    public: load('client.key.pub')
+exports.client_1 = {
+    private: load('client_1.key'),
+    public:  load('client_1.key.pub')
 };
 
-exports.client_tls = {
-    private: load('client_tls.key'),
-    public: load('client_tls.key.pub')
+exports.client_2 = {
+    private: load('client_2.key'),
+    public:  load('client_2.key.pub')
 };
--- test/module.vc.test.js
+++ test/module.vc.test.js
@@ -1,44 +1,77 @@
 const
-    {describe, test, before} = require('mocha'),
-    expect                   = require('expect'),
-    module_vc                = require('../src/module.vc.js'),
-    {generateKeyPair}        = require('jose/util/generate_key_pair'),
-    {fromKeyLike}            = require('jose/jwk/from_key_like'),
-    {decodeProtectedHeader}  = require('jose/util/decode_protected_header'),
-    testVC                   = Object.freeze(require('./data/example-verifiable-credential.json')),
-    testVP                   = Object.freeze(require('./data/example-verifiable-presentation.json'));
+    {describe, test, before}            = require('mocha'),
+    expect                              = require('expect'),
+    module_vc                           = require('../src/module.vc.js'),
+    {generateKeyPair}                   = require('jose/util/generate_key_pair'),
+    {fromKeyLike}                       = require('jose/jwk/from_key_like'),
+    {createPrivateKey, createPublicKey} = require('crypto'),
+    {decodeProtectedHeader}             = require('jose/util/decode_protected_header'),
+    testVC                              = Object.freeze(require('./data/example-verifiable-credential.json')),
+    testVP                              = Object.freeze(require('./data/example-verifiable-presentation.json')),
+    testCerts                           = require('./data/certs');
 
 describe('module.vc', function () {
 
-    const encryptOptions = {alg: 'RS256'}, decryptOptions = {jwks: []};
-    before('generate key pair', async function () {
-        const
-            {privateKey, publicKey} = await generateKeyPair(encryptOptions.alg),
-            [privateJWK, publicJWK] = await Promise.all([privateKey, publicKey].map(fromKeyLike));
+    const runtime = {};
+    before('generate keys and jwks', async function () {
+        const randomKeyPair = await generateKeyPair('RS256');
 
-        privateJWK.alg     = publicJWK.alg = encryptOptions.alg;
-        encryptOptions.kid = privateJWK.kid = publicJWK.kid = 'did:example:key-' + Date.now();
-        encryptOptions.jwk = Object.freeze(privateJWK);
-        decryptOptions.jwks.push(Object.freeze(publicJWK));
+        runtime.privateKeys = {
+            random:   randomKeyPair.privateKey,
+            server_1: createPrivateKey(testCerts.server_1.private),
+            server_2: createPrivateKey(testCerts.server_2.private)
+        };
+
+        runtime.publicKeys = {
+            random:   randomKeyPair.publicKey,
+            server_1: createPublicKey(testCerts.server_1.public),
+            server_2: createPublicKey(testCerts.server_2.public)
+        };
+
+        runtime.privateJwks = await Promise.all(
+            Object.entries(runtime.privateKeys)
+                .map(async ([kid, key]) => Object.assign(await fromKeyLike(key), {kid}))
+        );
+
+        runtime.publicJwks = await Promise.all(
+            Object.entries(runtime.publicKeys)
+                .map(async ([kid, key]) => Object.assign(await fromKeyLike(key), {kid}))
+        );
     });
 
     test('sign a VC as a JWT and verify it to the same VC again', async function () {
-        const jwt = await module_vc.sign_vc_as_jwt(testVC, encryptOptions);
+        const
+            sign_options = {alg: 'RS256', kid: 'random', key: runtime.privateKeys.random},
+            jwt          = await module_vc.sign_vc_as_jwt(testVC, sign_options);
+
         expect(typeof jwt).toBe('string');
         console.log(jwt);
-        expect(await decodeProtectedHeader(jwt)).toMatchObject({alg: encryptOptions.alg});
-        const vc = await module_vc.verify_vc_from_jwt(jwt, decryptOptions);
+        expect(await decodeProtectedHeader(jwt)).toMatchObject({alg: sign_options.alg});
+
+        const
+            // verify_options = {key: runtime.publicKeys.random},
+            verify_options = {jwks: runtime.publicJwks},
+            vc             = await module_vc.verify_vc_from_jwt(jwt, verify_options);
+
         expect(typeof vc).toBe('object');
         console.log(vc);
         expect(vc).toEqual(testVC);
     });
 
     test('sign a VP as a JWT and verify it to the same VP again', async function () {
-        const jwt = await module_vc.sign_vp_as_jwt(testVP, encryptOptions);
+        const
+            sign_options = {alg: 'RS256', kid: 'random', key: runtime.privateKeys.random},
+            jwt          = await module_vc.sign_vp_as_jwt(testVP, sign_options);
+
         expect(typeof jwt).toBe('string');
         console.log(jwt);
-        expect(await decodeProtectedHeader(jwt)).toMatchObject({alg: encryptOptions.alg});
-        const vp = await module_vc.verify_vp_from_jwt(jwt, decryptOptions);
+        expect(await decodeProtectedHeader(jwt)).toMatchObject({alg: sign_options.alg});
+
+        const
+            // verify_options = {key: runtime.publicKeys.random},
+            verify_options = {jwks: runtime.publicJwks},
+            vp             = await module_vc.verify_vp_from_jwt(jwt, verify_options);
+
         expect(typeof vp).toBe('object');
         console.log(vp);
         expect(vp).toEqual(testVP);
